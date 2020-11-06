dbc05eba7135b72a2a294d7c5a64221b3e7f9819
= <rachit.saxena42@gmail.com>
2020-11-06 11:41:47 +0530

    New one

---
+++ basic/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from . import views
+urlpatterns = (
+    path('', views.homepage , name='home'),
+
+    path('login/' , views.loginPage , name='loginPage'),
+    path('logout/' , views.logoutPage , name='logoutPage'),
+
+  
+    path('product/<str:pk>/' , views.product , name='product'),
+    path('callback/' , views.callBack , name='callBack'),
+    path('policy/' , views.policy , name='policy'),
+    path('about/' , views.about , name='about'),
+    path('protfolio-returns/', views.port , name='port'),
+)
